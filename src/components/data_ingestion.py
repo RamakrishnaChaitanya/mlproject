3af68f7b20a71c41e73a667aacd03efd6097ff38
Ramakrishna <rkchaitanya02@gmail.com>
2024-08-02 12:03:03 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,9 @@ import sklearn
 import numpy as np
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass # used to create class vairables
+from src.components.data_transformation import DataTransformation, DataTransformartionConfig
 
-# Ths file is Mianly to create train, test datasets
+# Ths file is Mianly to read data from a source and then to create train, test datasets
 
 @dataclass # decorator
 class DataIngestionConfig:
@@ -52,8 +53,14 @@ class DataIngestion:
             raise CustomException(e, sys)
 
 if __name__ == "__main__":
+    # obj = DataIngestion()
+    # obj.initiate_data_ingestion()
+
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
     
 
 
